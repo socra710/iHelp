430b0fcac6fa0d31f8ec39d183214cf1df9f75f3
김기형 <socra710@gmail.com>
2025-03-15 23:11:15 +0900

    csr loading 처리 수정

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -71,6 +71,29 @@ export default function Loading() {
           <div className="bg-gray mb-4" style={{ height: '100px', background: '#eee' }} />
         </article>
       </div>
+      <div className="grid grid-cols-1 gap-3">
+        <article className="bg-white p-6 rounded-lg shadow-md">
+          <div className="flex items-center mb-4">
+            <div
+              className="w-8 h-8 rounded-full cursor-pointer"
+              style={{ height: '32px', background: '#eee' }}
+            />
+            <span className="font-bold ml-1" style={{ width: '50px', background: '#eee' }}>
+              <span
+                className="font-normal text-gray-400 ml-2"
+                style={{ width: '50px', background: '#eee' }}
+              ></span>
+            </span>
+            <span className="font-bold ml-1" style={{ width: '80px', background: '#eee' }}>
+              <span
+                className="font-normal text-gray-400 ml-2"
+                style={{ width: '80px', background: '#eee' }}
+              ></span>
+            </span>
+          </div>
+          <div className="bg-gray mb-4" style={{ height: '100px', background: '#eee' }} />
+        </article>
+      </div>
     </div>
   );
 }
